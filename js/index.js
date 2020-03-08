bbfbc93603d8c70c6289b010b86b41e178406aa7
Alexsander <56883568+AlexsanderMr@users.noreply.github.com>
2020-03-08 13:52:10 +0200

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,36 @@
+//  console.log (Number("12"));
+//  "String"
+//  console.log (Number("random"));
+//  console.log (Number.parseInt(12));
+//  console.log (Number.parseInt(12.345));
+//  console.log(Number.parseFloat(12));
+//  console.log(Number.parseFloat(12.345));
+//  const number = Number("raw");
+//  console.log (parseInt("20px",2));
+//  console.log (Number.isNaN(number));
+// const isEqual=0.1+0.2==0.3;
+// console.log (0.1+0.2==0.3)
+// const isRightequal=(0.1*10+0.2*10)/10==(0.3*10)/10;
+// console.log ((0.1*10+0.2*10)/10==(0.3*10)/10);
+// console.log (Math.pow (2,16));
+// console.log (Math.floor(1.7));
+// console.log (Math.ceil(1.2));
+// console.log (Math.round(1.7));
+// console.log (Math.max(10,20,30,5,8,50));
+// console.log (Math.min(10,20,30,5,8,50));
+//  console.log (Math.random()*(10-1)+1);
+//  console.log (Math.random());
+// console.log("Javascript"[0]);
+// console.log("Javascript"[4]);
+// console.log("Javascript"[8]);
+// console.log(5+5+"5");
+// console.log(5+"5"+5);
+// const str= "Bla Bla Bla Bla "
+// console.log(str.length);
+// console.log(str.toUpperCase);
+// const str = "Hello World";
+// console.log(str.indexOf("World"));
+// const str = "Hello my Fuhrer"
+// const num = 10;
+// const res = num>5 && num<100;
+// console.log (res)
